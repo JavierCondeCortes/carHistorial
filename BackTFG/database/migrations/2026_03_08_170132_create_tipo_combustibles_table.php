3165781453084b5d57c2f721d45e3874722f9680
JavierCondeCortes <jccfmys@gmail.com>
2026-03-08 18:51:43 +0100

    saved file

--- BackTFG/database/migrations/2026_03_08_170132_create_tipo_combustibles_table.php
+++ BackTFG/database/migrations/2026_03_08_170132_create_tipo_combustibles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tipos_combustible', function (Blueprint $table) {
+        Schema::create('tipos_combustibles', function (Blueprint $table) {
             $table->id();
             $table->string('nombre')->unique(); // Ej: Diésel, Gasolina, Eléctrico
             $table->timestamps();
